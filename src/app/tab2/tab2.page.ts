804a28f4cbc4c3ea5ff55d170c16f1e3eb270e23
Cleiton Pessoa <cleitonpessoa53@hotmail.com>
2019-04-09 19:07:10 +0000

    Adicionado o controller do alert

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(private alertCtrl: AlertController){};
+
+  async showAdd(){
+    const alert = await this.alertCtrl.create({
+      header: 'O que deseja fazer?',
+      inputs: [
+        {
+          name: 'taskToDo',
+          type: 'text',
+          placeholder: 'Comprar pão',
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Click cancel');
+          }
+       }
+    ]
+    });
+  }
+
+}
